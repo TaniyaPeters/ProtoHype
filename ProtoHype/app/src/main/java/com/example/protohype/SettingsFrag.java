908149b9ac3c635f71cfae46931c8d2e9d27ea9c
Tork <atork@amd.com>
2019-05-19 06:19:44 -0400

    omg it just keeps crashing

--- ProtoHype/app/src/main/java/com/example/protohype/SettingsFrag.java
+++ ProtoHype/app/src/main/java/com/example/protohype/SettingsFrag.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,7 +38,7 @@ public class SettingsFrag extends Fragment implements View.OnClickListener {
                 startActivity(new Intent(getActivity(), MainActivity.class));
                 getActivity().finish();
                 break;
-            case R.id.view_profile_button:
+            case R.id.search_button:
                 mAuth.signOut();
                 startActivity(new Intent(getActivity(), PersonalActivity.class));
                 getActivity().finish();
