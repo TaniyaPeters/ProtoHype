0b25ec8d662dfbe5051e7229c30bcad8593f926b
TaniyaPeters <36174064+TaniyaPeters@users.noreply.github.com>
2019-05-19 07:21:45 -0400

    john's story

--- ProtoHype/app/src/main/java/com/example/protohype/SearchFrag.java
+++ ProtoHype/app/src/main/java/com/example/protohype/SearchFrag.java
@@ -1,7 +1,6 @@
 package com.example.protohype;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,8 +12,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,37 +19,14 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 
-public class SearchFrag extends Fragment implements View.OnClickListener {
-
-    private DatabaseReference mDatabase;
-    JSONObject jsonDatabase;
-
+public class SearchFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_search, container, false);
-        Button searchButton = v.findViewById(R.id.search_button);
-        searchButton.setOnClickListener(this);
-
-        Context context = this.getContext();
-        InputStream inputStream = context.getResources().openRawResource(R.raw.database);
-        String jsonString = new Scanner(inputStream).useDelimiter("\\A").next();
-
-        try{
-            jsonDatabase = new JSONObject(jsonString);
-        }
-        catch(JSONException e){
-            Log.w("Creating JSON", "Failed");
-        }
-
-        return v;
-
-    }
-
-    @Override
-    public void onClick(View v){
+        return inflater.inflate(R.layout.fragment_search, container, false);
 
 
     }
+
 }
 
