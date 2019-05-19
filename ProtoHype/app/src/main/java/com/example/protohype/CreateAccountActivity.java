1019e06e8c96694d2ade3d5413b575189cdabf05
Tork <atork@amd.com>
2019-05-19 03:05:52 -0400

    Can Create Email

--- ProtoHype/app/src/main/java/com/example/protohype/CreateAccountActivity.java
+++ ProtoHype/app/src/main/java/com/example/protohype/CreateAccountActivity.java
@@ -1,79 +1,55 @@
 package com.example.protohype;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity {
+
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_account);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    public void onCreateNewAccountPress(View v){
-        startActivity(new Intent(this, CreateAccountActivity.class));
-    }
-
-    public void loginWithEmail(View v){
+    public void onCreateNewAccount(View v){
         EditText emailText = findViewById(R.id.email_text_field);
         EditText passwordText = findViewById(R.id.password_field);
         String email = emailText.getText().toString();
         String password = passwordText.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password)
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d("EmailLoginButton", "signInWithEmail:success");
+                            Log.d("CreateUserButton", "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w("EmailLoginButton", "signInWithEmail:failure", task.getException());
+                            Log.w("CreateUserButton", "createUserWithEmail:failure", task.getException());
                             updateUI(null);
                         }
                     }
                 });
-    }
-
-    public void loginWithFacebook(View v){
-
-    }
-
-    public void loginWithGoogle(View v){
-
     }
 
     private void updateUI(FirebaseUser account){
